128f05923766b795027d2d4240a959d4fa118ec7
David Crespo <davidcrespoarroyo@gmail.com>
2016-04-12 08:51:04 +0100

    my first python exercise

---
+++ ex1.py
@@ -0,0 +1,7 @@
+flag = True
+if flag:
+	print "I usually use Sublime Text as Editor" #this is  comment in python
+else:
+	print "Hi world"
+	print "My name is David"
+	print "I am using textWrangler as Editor"
