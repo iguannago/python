32d153e5ced45aa99914a0e5c367d16cf860b74f
David Crespo <davidcrespoarroyo@gmail.com>
2016-05-30 16:24:30 +0100

    Added kgrs and cms

--- exercise5.py
+++ exercise5.py
@@ -7,8 +7,8 @@ my_teeth = 'White'
 my_hair = 'Brown'
 
 print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "He's %d inches tall and %f cms" % (my_height, my_height * 2.54)
+print "He's %d pounds heavy and %d kgrs" % (my_weight, my_weight * 0.453592)
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
 print "His teeth are usually %s depending on the coffee." % my_teeth
