f2d4f3036c3b0af9e3594f094cbfa871c0a46f16
David Crespo <davidcrespoarroyo@gmail.com>
2016-05-30 13:14:15 +0100

    Removed old file

--- ex1.py
+++
@@ -1,7 +0,0 @@
-flag = False
-if flag:
-	print "I usually use Sublime Text as Editor" #this is  comment in python
-else:
-	print "Hi world"
-	print "My name is David"
-	print "I am using textWrangler as Editor this time"
